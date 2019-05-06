436a0e363e7cd4801427114236fb483520cba4ca
Marshall Lochbaum <mwlochbaum@gmail.com>
2019-05-06 18:38:01 +0100

    Add combine/separate button (no effect on graphs yet)

--- graph.js
+++ graph.js
@@ -142,7 +142,7 @@ function showPhone(ph) {
     if (!ph.files) ph.files = fileNames(ph.phone);
     var l = ph.files.length;
     curves = [ph];
-    updatePhoneTable();
+    updatePhoneTable(l);
     Promise.all(ph.files.map(f=>d3.text(DIR+f))).then(function (frs) {
         var dat = frs.map(tsvParse);
         if (!path)
@@ -157,17 +157,24 @@ function showPhone(ph) {
         path.attr("d",line);
     });
 }
-function updatePhoneTable() {
+function updatePhoneTable(l) {
     var c = d3.select("#curves").selectAll("tr").data(curves,p=>p.brand+" "+p.phone);
     c.exit().remove();
     var f = c.enter().selectAll().data(p=>p.files.map(f=>[p,f])).enter().append("tr"),
         f0= f.filter((_,i)=>i===0),
-        one = () => f0.append("td").attr("rowspan",pf=>pf[0].files.length),
+        one = () => f0.append("td").attr("rowspan",l).attr("class","combined"),
         all = () => f.append("td");
     one().text(pf=>pf[0].brand+" "+pf[0].phone);
     all().text((_,i)=>["L","R"][i]);
 //  all().append("button").style("font-size","70%").text("hide");
-//  one().append("button").text("combine");
+    one().append("button").text("combine")
+        .on("click",function(){
+            var c = this.combined;
+            f0.selectAll(".combined").attr("rowspan",c?l:null);
+            f.filter((_,i)=>i!==0).style("visibility",c?null:"collapse");
+            d3.select(this).text(c?"combine":"separate");
+            this.combined=!c;
+        });
 }
 
 

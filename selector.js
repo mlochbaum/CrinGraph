1dbc58f098dd79c4b8d135aee79943d757078714
Marshall Lochbaum <mwlochbaum@gmail.com>
2019-05-08 21:38:00 +0100

    Correct volume averaging calculation

--- selector.js
+++ selector.js
@@ -6,9 +6,9 @@ var tsvParse = fr => d3.tsvParseRows(fr).slice(2,482);
 function flatten(l) { return [].concat.apply([],l); }
 function avgCurves(curves) {
     return curves
-        .map(c=>c.map(d=>Math.exp(d[1])))
+        .map(c=>c.map(d=>Math.pow(10,d[1]/20)))
         .reduce((as,bs) => as.map((a,i) => a+bs[i]))
-        .map((x,i) => [curves[0][i][0], Math.log(x/curves.length)]);
+        .map((x,i) => [curves[0][i][0], 20*Math.log10(x/curves.length)]);
 }
 
 var activePhones = [];

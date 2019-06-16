b79a872056a86241f2d8d25cd257969eb94e61b7
Marshall Lochbaum <mwlochbaum@gmail.com>
2019-06-16 07:54:24 -0600

    Unhide curve whenever key icon is clicked

--- selector.js
+++ selector.js
@@ -210,17 +210,19 @@ function updatePhoneTable() {
     td().attr("class","button").html("BASE<br>-LINE")
         .on("click", p => setBaseline(p===baseline.p ? baseline0
                                                      : getBaseline(p)));
+    function toggleHide(p) {
+        var h = p.hide;
+        var t = table.selectAll("tr").filter(q=>q===p);
+        t.select(".keyLine").on("click", h?null:toggleHide)
+            .select("path").attr("opacity", h?null:0.5);
+        t.select(".hideIcon").classed("selected", !h);
+        gpath.selectAll("path").filter(c=>c.p===p)
+            .attr("opacity", h?null:0);
+        p.hide = !h;
+    }
     td().attr("class","button hideIcon")
         .html("<svg viewBox='0 0 14 12'><path d='M2 6Q7 0 12 6Q7 12 2 6Z' stroke-width='1' stroke='currentColor' fill='none'/><circle cx='7' cy='6' r='2' stroke='none' fill='currentColor'/><line stroke-width='1' x1='4.4' y1='10.3' x2='10.4' y2='2.3' stroke='white'/><line stroke-width='1' x1='3.6' y1= '9.7' x2= '9.6' y2='1.7' stroke='currentColor'/></svg>")
-        .on("click",function(p){
-            var h = p.hide;
-            table.selectAll("tr").filter(q=>q===p)
-                .select(".keyLine").select("path").attr("opacity", h?null:0.5);
-            d3.select(this).classed("selected", !h);
-            gpath.selectAll("path").filter(c=>c.p===p)
-                .attr("opacity", h?null:0);
-            p.hide = !h;
-        });
+        .on("click", toggleHide);
     td().attr("class","button").text("PIN")
         .on("click",function(p){
             p.pin = true; nextPN = null;

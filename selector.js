319f747dd10a5966e7324644b277df9da73a2e1d
Marshall Lochbaum <mwlochbaum@gmail.com>
2019-06-19 05:36:49 +0100

    Use svg <use> icon for hide button

--- selector.js
+++ selector.js
@@ -223,7 +223,7 @@ function updatePhoneTable() {
         p.hide = !h;
     }
     td().attr("class","button hideIcon")
-        .html("<svg viewBox='0 0 14 12'><path d='M2 6Q7 0 12 6Q7 12 2 6Z' stroke-width='1' stroke='currentColor' fill='none'/><circle cx='7' cy='6' r='2' stroke='none' fill='currentColor'/><line stroke-width='1' x1='4.4' y1='10.3' x2='10.4' y2='2.3' stroke='white'/><line stroke-width='1' x1='3.6' y1= '9.7' x2= '9.6' y2='1.7' stroke='currentColor'/></svg>")
+        .html("<svg viewBox='0 0 14 12'><use xlink:href='#hide-icon'></use></svg>")
         .on("click", toggleHide);
     td().attr("class","button")
         .on("click",function(p){

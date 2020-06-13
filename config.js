7bcba4a93168233c31f0d70facff1afdfcf9489f
Marshall Lochbaum <mwlochbaum@gmail.com>
2020-06-13 14:12:54 -0400

    Use a generic watermark for sample pages

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ function watermark(svg) {
     let wm = svg.append("g")
         .attr("transform", "translate("+(pad.l+W/2)+","+(pad.t+H/2-20)+")")
         .attr("opacity",0.2);
-    wm.append("image")
-        .attrs({x:-64, y:-64, width:128, height:128, "xlink:href":"https://crinacle.com/wp-content/uploads/2019/10/crinacle.png"});
+//  wm.append("image")
+//      .attrs({x:-64, y:-64, width:128, height:128, "xlink:href":URL});
     wm.append("text")
-        .attrs({x:0, y:100, "font-size":36, "text-anchor":"middle"})
-        .text("crinacle.com/graphs");
+        .attrs({x:0, y:40, "font-size":40, "text-anchor":"middle"})
+        .text("sample graphs");
 }
 
 const max_channel_imbalance = 5;

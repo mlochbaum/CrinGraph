a7151bc44cdef158577a110c1aa54e00312e20f0
Marshall Lochbaum <mwlochbaum@gmail.com>
2021-05-15 14:07:56 -0400

    Simplify target line styling code

--- graphtool.js
+++ graphtool.js
@@ -1025,21 +1025,12 @@ function updatePaths() {
     clearLabels();
     let c = d3.merge(activePhones.map(p => p.activeCurves)),
         p = gpath.selectAll("path").data(c, d=>d.id);
-    
-    if ( !targetColorCustom ) {
-        p.join("path").attr("opacity", c=>c.p.hide?0:null)
-            .classed("sample", c=>c.p.samp)
-            .attr("stroke", getColor_AC).call(redrawLine)
-            .filter(c=>c.p.isTarget)
-            .style("stroke-dasharray", function(){ if (targetDashed) {return ("6, 3")} else { return null } });
-    } else {
-        p.join("path").attr("opacity", c=>c.p.hide?0:null)
-            .classed("sample", c=>c.p.samp)
-            .attr("stroke", getColor_AC).call(redrawLine)
-            .filter(c=>c.p.isTarget)
-            .style("stroke-dasharray", function(){ if (targetDashed) {return ("6, 3")} })
-            .attr("stroke", targetColorCustom);
-    }    
+    let t = p.join("path").attr("opacity", c=>c.p.hide?0:null)
+        .classed("sample", c=>c.p.samp)
+        .attr("stroke", getColor_AC).call(redrawLine)
+        .filter(c=>c.p.isTarget);
+    if (targetDashed) t.style("stroke-dasharray", "6, 3");
+    if (targetColorCustom) t.attr("stroke", targetColorCustom);
     if (ifURL) addPhonesToUrl();
 }
 let colorBar = p=>'url(\'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 8"><path d="M0 8v-8h1c0.05 1.5,-0.3 3,-0.16 5s0.1 2,0.15 3z" fill="'+getBgColor(p)+'"/></svg>\')';

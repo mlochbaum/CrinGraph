e7a73bafa8009a48b2ce000c234413641e3e2ef9
Mark Ryan Sallee <markryansallee@gmail.com>
2021-05-30 11:26:04 -0700

    Stripped GTM integration
    Left events and variable setting, which could be hooked up to any analytics platform.

--- graphAnalytics.js
+++ graphAnalytics.js
@@ -1,28 +1,5 @@
-// Set these variables to your own GTM ID and site name
 let analyticsSite = "Generic Graph site",           // Site name for attributing analytics events to your site
-    analyticsGtmId = "GTM-0000000",                 // GTM ID used for analytics. If you don't already have one, you'' need to create a Google Tag Manager account
     logAnalytics = true;                            // If true, events are logged in console
-
-// Load Google Tag Manager onto the page
-function setupGraphAnalytics() {
-    const gtmScriptContent = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','"+ analyticsGtmId +"');",
-        gtmIframeSrc = "https://www.googletagmanager.com/ns.html?id="+ analyticsGtmId,
-        gtmIframeStyle = "display: none; visibility: hidden;",
-        graphAnalyticsSrc = "graphAnalytics.js";
-
-    const pageHead = document.querySelector("head"),
-          pageBody = document.querySelector("body"),
-          gtmScript = document.createElement("script"),
-          gtmNoscript = document.createElement("noscript"),
-          gtmIframe = document.createElement("iframe"),
-          graphAnalytics = document.createElement("script");
-
-    gtmScript.textContent = gtmScriptContent;
-    pageHead.prepend(gtmScript);
-}
-setupGraphAnalytics();
-
-window.dataLayer = window.dataLayer || [];
 
 
 
@@ -37,18 +14,12 @@ function pushPhoneTag(eventName, p, trigger) {
         phoneModel = p.dispName,
         value = 1;
     
-    window.dataLayer.push({
-        "event" : eventName,
-        "trigger" : eventTrigger,
-        "site": analyticsSite,
-        "phoneBrand": phoneBrand,
-        "phoneModel": phoneModel,
-        "phoneName" : phoneBrand + ' ' + phoneModel,
-        "value": value
-    });
+    // Write function here to push event with the values described above
     
     if (logAnalytics) { console.log("Event:      "+ eventName +"\nTrigger:    "+ eventTrigger +"\nSite:       "+ analyticsSite +"\nPhone:      "+ phoneBrand +" "+ phoneModel); }
 }
+
+
 
 // For events not related to a specific phone, e.g. user clicked screenshot button
 function pushEventTag(eventName, targetWindow, trigger) {
@@ -57,16 +28,12 @@ function pushEventTag(eventName, targetWindow, trigger) {
         par = "?share=",
         value = 1,
         activePhones = url.includes(par) ? decodeURI(url.replace(/_/g," ").split(par).pop().replace(/,/g, ", ")) : "null";
-    
-    window.dataLayer.push({
-        "event" : eventName,
-        "trigger" : eventTrigger,
-        "site": analyticsSite,
-        "activePhones": activePhones,
-        "value": value
-    });
+        
+    // Write function here to push event with the values described above
     
     if (logAnalytics) { console.log("Event:      "+ eventName +"\nTrigger:    "+ eventTrigger +"\nSite name:  "+ analyticsSite +"\nActive:     "+activePhones); }
 }
+
+
 
 if (logAnalytics) { console.log("... Analytics initialized ... "); }
